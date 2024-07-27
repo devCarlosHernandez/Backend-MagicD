825553b652b2367b09c7bb9a0cec0e0128e65af6
Carlos Hernandez <juan.carlos.jchc8@gmail.com>
2024-07-26 21:10:51 -0600

    Backend create and configured for java 17

---
+++ magic_backend/src/main/java/com/springboot/backend/magic/magic_backend/MagicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.backend.magic.magic_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicBackendApplication.class, args);
+	}
+
+}
